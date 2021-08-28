0cc0e69dccc89bb4aebab887f3963607416562ff
joshua-gnd <joshua@sonover.com>
2021-08-27 20:00:05 -0400

    ratings and tasks' creation troubleshoot

--- server/models/Task.js
+++ server/models/Task.js
@@ -8,10 +8,11 @@ const TaskSchema = new Schema({
     },
     category: { type: String, default: "Other" },
     ratings: [{
-        id: Number,
-        rating: Number
+        type: Schema.Types.ObjectId,
+        ref: 'rating',
+        autopopulate: true
     }]
-}),
+}).plugin(require('mongoose-autopopulate'));
 
     Task = mongoose.model('task', TaskSchema)
 
